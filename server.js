b1a3d97dff5524ccf84622c0363529ecc7b96ce6
ThomasJasonM1 <jason@call-em-all.com>
2020-05-31 23:53:29 -0500

    Fixes a path issue

--- server.js
+++ server.js
@@ -4,16 +4,16 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3002;
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 app.get("/pdf", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/assets/files/Profile.pdf"));
+    // res.sendFile(path.join(__dirname, "./public/assets/files/Profile.pdf"));
 });
 
 app.listen(PORT, function () {
